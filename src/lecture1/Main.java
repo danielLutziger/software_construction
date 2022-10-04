02de393e8b0883e870c4f3b2e99893f9735c4ade
dani <lutzidan@students.zhaw.ch>
2022-10-04 16:08:31 +0200

    lecture 2

--- src/lecture1/Main.java
+++ src/lecture1/Main.java
@@ -2,6 +2,10 @@ package lecture1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(eat("apple"));
+    }
+
+    static String eat(String item) {
+        return String.format("I ate a %s, and it was delicious!",item); 
     }
 }
